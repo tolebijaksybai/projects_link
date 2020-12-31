31d285df30d4c572a2ce779470fb5c9fcc86b8f2
Tolebi Zhaksybai <52714747+tolebijaksybai@users.noreply.github.com>
2021-01-01 02:58:10 +0600

    Add files via upload

---
+++ build/precache-manifest.e9da7928f6c55703d57420a253b0963d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c7d066a8cbecc61c31546a7e9d7c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f75a90720ef50593f9fb",
+    "url": "/static/css/main.d2ea92c4.chunk.css"
+  },
+  {
+    "revision": "d4a0e77bb43974aa0a51",
+    "url": "/static/js/2.c26e76ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c26e76ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75a90720ef50593f9fb",
+    "url": "/static/js/main.fff72eae.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
